1300855cfd6993b41212fa0555feb477fe4cbfdf
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2013-10-29 02:10:38 +0000

    Update API key

--- OsmAnd/src/com/kinatomicEsus/plus/ExtensionDownloaderService.java
+++ OsmAnd/src/com/kinatomicEsus/plus/ExtensionDownloaderService.java
@@ -4,11 +4,9 @@ import com.google.android.vending.expansion.downloader.impl.DownloaderService;
 
 public class ExtensionDownloaderService extends DownloaderService {
     // You must use the public key belonging to your publisher account
-    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAk0BASiqccaDOSy8CIvsn/sJh7vcWObSOrgDUshrok55naYV0mAC7YJfKnHA9BChrZmX8k25bYL0bwtmza/T0wTfGtKSA9YCDPeFl1aH6QMRpOBtPJMQ/pI/6YAuwwHLDLQQphLcHEss0hw/6q8pVAOAxiJhWLkJWnKC0Lm33WeiX97oQZocuSJ2bFoFlm+curBd5Oj/1udkVinCCEHwrMmQeajnzoc6X/LcsN8P8RJFxC+mJNbdfcT4ItN2YHE8EfWuNVLAWzsjdJxSM1O5Hcn/5q9g0zV52F80Cdlws2XY1PBNcR5aVPca/rwq+hJy7IANgqEcz9DRQxOjYy3yEvQIDAQAB";
+    public static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAloM6XAiNwK9f6HWplfN6J+5WpdlYeuVqzPJtWUKp/AkuYAnKV3LjOPuo2egTrrycFYBx6GiwuUaTmZb/GNztWx+FRT9GJpJNEXplWs5SvSxY/6MVUpQndyXAqiqmscBpz7NcGDcsdLwheZKlBpd5YR5YfSpkwgstHmKjKtix0m/ns2CLoKq/b1Rp4iqiENqh1pDohx3hFZ9DHxIyQaQNi5IJt/DbkhucG07PS8lOb6ZlSGcFDuuaFcDfBGRDq5osfNr/fW837OEatYTl0TztnqnYh/dAdKnXt5uZA+XD6vZV9RPwVIk82EVxDAKm1URuEB1ZQo60TSPI/zaUF4txiwIDAQAB";
     // You should also modify this salt
-    public static final byte[] SALT = new byte[] { 5, 3, -10, 2, 52, -3,
-            44, -22, 35, 21, -30, -60, 57, 35, -120, -55, -32, 34, -5, 45
-    };
+    public static final byte[] SALT = new byte[] { 73, -35, -110, -44, 59, -19, 52, 5, -26, -83, -45, -7, 32, -86, -67, -124, 103, -116, -74, 25 };
 
     @Override
     public String getPublicKey() {
